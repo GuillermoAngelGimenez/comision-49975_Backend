8f0aa10448650315a09338726c0f255cbf039df2
GuillermoAngelGimenez <angel_gag@hotmail.com>
2023-11-08 23:55:31 -0300

    cambios en la clase

--- 1raEntrega/src/productManager.js
+++ 1raEntrega/src/productManager.js
@@ -16,8 +16,52 @@ class ProductManager {
   }
 
   async addProduct(newProduct) {
+    let { title, description, price, thumbnail, code, stock } = newProduct;
+
+    //Validar Code
     let products = await this.getProducts();
+
+    let id = 1;
+    if (products.length > 0) {
+      id = products[products.length - 1].id + 1;
+
+      //Se valida que el código no se repita
+      let existe = products.find((u) => u.code === code);
+      if (existe) {
+        console.log(
+          `No se puede agregar el producto "${title} - ${description}" porque el código "${code}" ya se encuentra registrado`
+        );
+        return;
+      }
+
+      //Se valida que se ingresen todos los campos requeridos para el producto
+      if (
+        title === undefined ||
+        description === undefined ||
+        price === undefined ||
+        // thumbnail === undefined ||
+        code === undefined ||
+        stock === undefined
+      ) {
+        console.log(
+          `No se ingresaron todos los valores requeridos para el producto. \nValores ingresados: \n\ttitle: ${title}, description: ${description}, price: ${price}, thumbnail: ${thumbnail}, code: ${code}, stock: ${stock}`
+        );
+        return;
+      }
+    }
+
+    // let newProduct = {
+    //   id,
+    //   title,
+    //   description,
+    //   price,
+    //   thumbnail,
+    //   code,
+    //   stock
+    // };
+
     products.push(newProduct);
+
     await fs.promises.writeFile(this.path, JSON.stringify(products, null, 5));
   }
 
@@ -50,13 +94,13 @@ class ProductManager {
 
     let products = await this.getProducts();
 
-    // const indexProduct = products.findIndex((product) => product.id === id);
-    // if (indexProduct === -1) {
-    //   console.error(
-    //     `El producto con id ${id} no se encuentra registrado. No es posible actualizar el producto.`
-    //   );
-    //   return;
-    // }
+    const indexProduct = products.findIndex((product) => product.id === id);
+    if (indexProduct === -1) {
+      console.error(
+        `El producto con id ${id} no se encuentra registrado. No es posible actualizar el producto.`
+      );
+      return;
+    }
 
     // if (products[indexProduct].hasOwnProperty(campo)) {
     //   products[indexProduct][campo] = valor;
